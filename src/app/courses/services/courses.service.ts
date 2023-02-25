2a430951efe6c582f5336f2882e98a12dc5d444a
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2023-02-25 16:47:10 -0500

    inicializacao frontend

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private readonly API = 'api-v1/courses';
+
+  constructor(private httpClient: HttpClient) {}
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
+  }
+}
